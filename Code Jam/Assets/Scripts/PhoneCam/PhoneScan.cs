9f4511139c540d78af2dbab0f34953e6b45be9a5
ChocZeChoc <115222117+ChocZeChoc@users.noreply.github.com>
2023-04-26 13:58:48 +0200

    Add scanning animation

--- Code Jam/Assets/Scripts/PhoneCam/PhoneScan.cs
+++ Code Jam/Assets/Scripts/PhoneCam/PhoneScan.cs
@@ -1,15 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PhoneScan : MonoBehaviour
 {
-    private GameObject scanner;
-
+    [SerializeField] private GameObject scanner;
+    [SerializeField] private Animator animator;
     private void Start()
     {
-        scanner = this.gameObject;
+        animator = scanner.GetComponent<Animator>();
     }
     
+    
+    public void buttonScan()
+    {
+        StartCoroutine(StartScan());
+    }
 
+    private IEnumerator StartScan()
+    {
+        animator.SetBool("Scanning", true);
+        yield return new WaitForSeconds(10f);
+        animator.SetBool("Scanning",false);
+    }
 }
