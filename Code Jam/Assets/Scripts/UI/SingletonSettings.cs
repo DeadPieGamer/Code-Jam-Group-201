acee1e200ecd88adb8a709aaaa565d3b894724cc
DeadPieGamer <67107773+DeadPieGamer@users.noreply.github.com>
2023-04-27 14:09:05 +0200

    Base stuff for singleton set up, probably needs testing and sprites though

---
+++ Code Jam/Assets/Scripts/UI/SingletonSettings.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SingletonSettings : MonoBehaviour
+{
+    public static SingletonSettings instance;
+
+    [SerializeField, Tooltip("What Book Scenes are called")] private string bookNamingConvention = "Bog Scene ";
+
+    [SerializeField, Tooltip("The pause menu")] private GameObject pauseMenu;
+    [SerializeField, Tooltip("The pause button")] private Button pauseButton;
+    
+    private bool canPause = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // If this script doesn't already exist
+        if (instance == null)
+        {
+            // Make this one gameObject exist throughout all scenes
+            DontDestroyOnLoad(gameObject);
+            instance = this;
+        }
+        else
+        {
+            // If this already exists, kill any extra copies of it that may appear
+            Destroy(gameObject);
+        }
+    }
+
+    public void TogglePauseMenu()
+    {
+        pauseMenu.SetActive(!pauseMenu.activeSelf);
+    }
+
+    public void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        // If it's a book scene, don't appear. Else appear in the corner
+        canPause = !scene.name.Contains(bookNamingConvention);
+        pauseButton.interactable = canPause;
+    }
+
+    #region Scene Loading Events Setup
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+    #endregion
+}
