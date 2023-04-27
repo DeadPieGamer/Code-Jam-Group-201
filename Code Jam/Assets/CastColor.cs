72cb29379fbfc9afb1888a6508e904c191390c00
Carolinebchr <115777972+Carolinebchr@users.noreply.github.com>
2023-04-27 13:27:07 +0200

    Remember the picked color and go to next scene after picking a color

--- Code Jam/Assets/CastColor.cs
+++ Code Jam/Assets/CastColor.cs
@@ -3,28 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ChangeColor : MonoBehaviour
+public class CastColor : MonoBehaviour
 {
+  
     // Code inspired by "GameAssetWorld", YT link: https://www.youtube.com/watch?v=EzZGPRBchJo
 
-    public Image image;
     public Sprite sprite;
     public Sprite[] sprites;
-
-    public Animator okAnimator;
-
-    public Button okButton;
+    public SpriteRenderer castRender;
+   
 
     public void colorChange(int button)
     {
         sprite = sprites[button];
-        image.sprite = sprite;
-        PlayerPrefs.SetInt("Picked Color", button);
-        okAnimator.SetBool("Moving", true);
-        okButton.enabled = true;
+        castRender.sprite = sprite;
     }
     private void Start()
     {
-        okButton.enabled = false;
+        colorChange(PlayerPrefs.GetInt("Picked Color"));
     }
 }
+
---
+++ Code Jam/Assets/Scripts/ChangeColorCast/Cast_Okbutton.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cast_Okbutton : MonoBehaviour
+{
+    public void Okpressed()
+    {
+        
+    }
+}
