7d7e5fe792180f1eb42218d651e95c2d906928b0
ChocZeChoc <115222117+ChocZeChoc@users.noreply.github.com>
2023-04-28 11:02:38 +0200

    added comments to Phonecam

--- Code Jam/Assets/Scripts/PhoneCam/PhoneCam.cs
+++ Code Jam/Assets/Scripts/PhoneCam/PhoneCam.cs
@@ -17,16 +17,16 @@ public class PhoneCam : MonoBehaviour
 
     private void Start()
     {
-        WebCamDevice[] devices = WebCamTexture.devices;
+        WebCamDevice[] devices = WebCamTexture.devices;//get all camerasdevieces in an array
 
-        if (devices.Length == 0 )
+        if (devices.Length == 0 )//if it cant find any cameras it does nothing & set camAvailable bool to false
         {
             Debug.Log("No camera detected");
             camAvailable = false;
             return;
         }
 
-        for(int i = 0; i < devices.Length; i++)
+        for(int i = 0; i < devices.Length; i++) // for every devieces that was found if it's not a front camera it set the size and the camera to what it found
         {
             if (!devices[i].isFrontFacing)
             {
@@ -35,13 +35,13 @@ public class PhoneCam : MonoBehaviour
             }
         }
 
-        if (backcam == null)
+        if (backcam == null)//if there're no backcam it does nothing
         {
             Debug.Log("Unable to find back Camera");
             return;
         }
 
-        backcam.Play();
+        backcam.Play();//display what the camera see on to the BG object
         background.texture = backcam;
 
         camAvailable = true;
@@ -51,10 +51,10 @@ public class PhoneCam : MonoBehaviour
     {
         if (!camAvailable) return;
         
-        float ratio = (float)backcam.width / (float)backcam.height;
+        float ratio = (float)backcam.width / (float)backcam.height;//change the ratio of the BG object to the same size as the camera.
         fit.aspectRatio = ratio;
 
-        float scaleY = backcam.videoVerticallyMirrored ? -1f : 1f;
+        float scaleY = backcam.videoVerticallyMirrored ? -1f : 1f; //if the camera is upsidedown it get's flipped
         background.rectTransform.localScale = new Vector3(1f, scaleY, 1f);
 
         //int orient = -backcam.videoRotationAngle;
