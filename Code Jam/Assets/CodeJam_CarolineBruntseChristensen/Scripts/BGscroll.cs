0fb44145888f0839662046c99c88580cfedab02f
Carolinebchr <115777972+Carolinebchr@users.noreply.github.com>
2023-05-04 15:04:23 +0200

    Made a BGscroll script and started the code

---
+++ Code Jam/Assets/CodeJam_CarolineBruntseChristensen/Scripts/BGscroll.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGscroll : MonoBehaviour
+{
+    public float speed = 4f;
+    private Vector3 StartPosition;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
