b605313db6eabb82ac31c1ed62fd36df236a6152
Carolinebchr <dkcbch@gmail.com>
2023-05-06 16:04:41 +0200

    Made the BG loop

--- Code Jam/Assets/CodeJam_CarolineBruntseChristensen/Scripts/BGscroll.cs
+++ Code Jam/Assets/CodeJam_CarolineBruntseChristensen/Scripts/BGscroll.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class BGscroll : MonoBehaviour
 {
+    // Code inspired by "Adamant Algorithm" on YT link: https://www.youtube.com/watch?v=U72trwZ7AT8&list=LL&index=1&t=185s
+   
     public float speed = 4f;
     private Vector3 StartPosition;
 
@@ -11,12 +13,17 @@ public class BGscroll : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        StartPosition = transform.position;
+        StartPosition = transform.position; //When you start the game the BG would be
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector3.down * speed * Time.deltaTime); //This will move the background downwards
+        if(transform.position.y < -14.80f) //the moment the original BG image goes any value below -14.80 the position of it is reset to the same place that it was at the start of the game
+        { 
+            transform.position = StartPosition;
+        }
     }
 }
